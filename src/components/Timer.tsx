7e84e7e638da5fbc2d4ac834dd83370bf907a96a
Arman <37100541+CarboxyDev@users.noreply.github.com>
2023-06-22 15:55:40 +0530

    Create a working timer

--- src/components/Timer.tsx
+++ src/components/Timer.tsx
@@ -1,11 +1,33 @@
 'use client';
 
-import { useState } from 'react';
+import { useStore } from '@/lib/store/useStore';
+import { useTimer } from '@/lib/store/useTimer';
+import { useEffect } from 'react';
 
 const Timer = () => {
-  const [time, setTime] = useState(0);
+  const persistentTimer = useStore(useTimer, (state) => state);
 
-  return <div></div>;
+  useEffect(() => {
+    const clientCalculateTime = setInterval(() => {
+      persistentTimer?.setDuration(persistentTimer?.duration + 1);
+      console.log(`Timer: ${persistentTimer?.duration}`);
+      if (
+        typeof persistentTimer?.duration != 'undefined' &&
+        persistentTimer?.duration % 10 === 0 &&
+        persistentTimer?.duration != 0
+      ) {
+        persistentTimer.reset();
+      }
+    }, 1000);
+
+    return () => clearInterval(clientCalculateTime);
+  }, [persistentTimer]);
+
+  return (
+    <div>
+      <div>Time: {persistentTimer?.duration}</div>
+    </div>
+  );
 };
 
 export default Timer;
--- src/lib/store/useTimer.ts
+++ src/lib/store/useTimer.ts
@@ -3,6 +3,7 @@ import { persist } from 'zustand/middleware';
 
 interface Timer {
   duration: number;
+  setDuration: (timeInSeconds: number) => void;
   increment: (amount: number) => void;
   reset: () => void;
 }
@@ -11,6 +12,8 @@ export const useTimer = create<Timer>()(
   persist(
     (set) => ({
       duration: 0,
+      setDuration: (timeInSeconds: number) =>
+        set((state) => ({ duration: timeInSeconds })),
       increment: (amount: number) =>
         set((state) => ({ duration: state.duration + amount })),
       reset: () => set({ duration: 0 }),
