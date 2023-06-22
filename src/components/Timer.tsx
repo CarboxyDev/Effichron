bf0713f38a3f6a4f13d0725a2d6ab4a3923dd72f
Arman <37100541+CarboxyDev@users.noreply.github.com>
2023-06-22 16:52:04 +0530

    Add start/pause and reset functionality for simple timer

--- src/components/Timer.tsx
+++ src/components/Timer.tsx
@@ -2,30 +2,40 @@
 
 import { useStore } from '@/lib/store/useStore';
 import { useTimer } from '@/lib/store/useTimer';
-import { useEffect } from 'react';
+import { useEffect, useState } from 'react';
 
 const Timer = () => {
   const persistentTimer = useStore(useTimer, (state) => state);
+  const [activeTimer, setActiveTimer] = useState(true);
 
   useEffect(() => {
     const clientCalculateTime = setInterval(() => {
-      persistentTimer?.setDuration(persistentTimer?.duration + 1);
-      console.log(`Timer: ${persistentTimer?.duration}`);
-      if (
-        typeof persistentTimer?.duration != 'undefined' &&
-        persistentTimer?.duration % 10 === 0 &&
-        persistentTimer?.duration != 0
-      ) {
-        persistentTimer.reset();
+      if (activeTimer) {
+        persistentTimer?.setDuration(persistentTimer?.duration + 1);
+        console.log(`Timer: ${persistentTimer?.duration}`);
       }
     }, 1000);
 
     return () => clearInterval(clientCalculateTime);
-  }, [persistentTimer]);
+  }, [persistentTimer, activeTimer]);
 
   return (
-    <div>
+    <div className="flex flex-col justify-center items-center">
       <div>Time: {persistentTimer?.duration}</div>
+      <div className="mt-4 gap-x-2 flex flex-row">
+        <button
+          className="px-4 py-2 bg-blue-500 text-white rounded-lg"
+          onClick={() => setActiveTimer(!activeTimer)}
+        >
+          {activeTimer ? 'Pause' : 'Start'}
+        </button>
+        <button
+          className="px-4 py-2 bg-slate-500 text-white rounded-lg"
+          onClick={() => persistentTimer?.reset()}
+        >
+          Reset
+        </button>
+      </div>
     </div>
   );
 };
