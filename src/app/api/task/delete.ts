1208b08239e8fa1b8e01e5ae20934cb99b1f56c4
Arman <37100541+CarboxyDev@users.noreply.github.com>
2023-08-06 22:00:53 +0530

    Refactor: Add lib/auth.ts for simplifying auth during api requests + Work on DELETE task operation

--- src/app/api/task/delete.ts
+++ src/app/api/task/delete.ts
@@ -1,5 +1,21 @@
+import { getUserFromSession } from '@/lib/auth';
 import { SendResponse } from '@/utils/api';
+import { getServerSession } from 'next-auth';
+import { authOptions } from '../auth/[...nextauth]/authOptions';
 
 export async function DELETE_TASK(req: Request, res: Response) {
-  return SendResponse('Not functioning yet', 200);
+  const session = await getServerSession(authOptions);
+  const getUser = await getUserFromSession(session, {
+    notLoggedIn: 'You have to be logged in to delete tasks',
+    invalidAccount: 'You do not have a valid account',
+  });
+
+  const user = getUser.user;
+
+  if (!user) {
+    return getUser.errorResponse;
+  }
+
+  console.log('[DB] Deleted task');
+  return SendResponse('Deleted the task', 200);
 }
---
+++ src/lib/auth.ts
@@ -0,0 +1,63 @@
+import { SendResponse } from '@/utils/api';
+import { Session } from 'next-auth';
+import { prisma } from './prisma';
+
+interface GetUserErrorMessages {
+  notLoggedIn?: string; // No user session found
+  invalidAccount?: string; // No user record found in database
+}
+
+interface GetUserFromSessionReturn {
+  user: {
+    id: string;
+    name?: string | null;
+    email?: string | null;
+    image?: string | null;
+  } | null;
+  errorResponse: Response; // This should only be used when there's an error in user login flow
+}
+
+// ! This method uses the user's email to find the user in the database. It is possible (although rare) that the user may not have a linked email, handle that case if possible.
+export const getUserFromSession = async (
+  session: Session | null,
+  errorMessages: GetUserErrorMessages
+): Promise<GetUserFromSessionReturn> => {
+  if (!session || !session.user) {
+    return {
+      user: null,
+      errorResponse: SendResponse(
+        errorMessages.notLoggedIn || 'You have to be logged in to do this',
+        401
+      ),
+    };
+  }
+
+  const user = session.user;
+
+  const prismaUser = await prisma.user.findUnique({
+    where: {
+      email: user.email as string,
+    },
+  });
+
+  if (!prismaUser) {
+    return {
+      user: null,
+      errorResponse: SendResponse(
+        errorMessages.invalidAccount || 'You do not have a valid account',
+        403
+      ),
+    };
+  }
+
+  const userId = prismaUser.id;
+  return {
+    user: {
+      id: userId,
+      name: user.name,
+      email: user.email,
+      image: user.image,
+    },
+    errorResponse: SendResponse('OK', 200),
+  };
+};
