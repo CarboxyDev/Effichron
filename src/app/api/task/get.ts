fbbb6fea6fb3ec5d23d67dd13b1286587d53b05d
Arman <37100541+CarboxyDev@users.noreply.github.com>
2023-08-07 22:42:53 +0530

    Implement fetching tasks from server in /tasks

--- src/app/api/task/get.ts
+++ src/app/api/task/get.ts
@@ -1,5 +1,31 @@
+import { getUserFromSession } from '@/lib/auth';
+import { prisma } from '@/lib/prisma';
 import { SendResponse } from '@/utils/api';
+import { getServerSession } from 'next-auth';
+import { authOptions } from '../auth/[...nextauth]/authOptions';
 
 export async function GET_TASK(req: Request, res: Response) {
-  return SendResponse('Not functioning yet', 200);
+  const session = await getServerSession(authOptions);
+  const getUser = await getUserFromSession(session, {
+    notLoggedIn: 'You have to be logged in to view tasks',
+    invalidAccount: 'You do not have a valid account to view tasks',
+  });
+
+  const user = getUser.user;
+  if (!user) {
+    return getUser.errorResponse;
+  }
+
+  try {
+    const tasks = await prisma.task.findMany({
+      where: {
+        userId: user.id,
+      },
+    });
+    console.log('[+] GET tasks');
+    return new Response(JSON.stringify(tasks), { status: 200 });
+  } catch (error) {
+    console.log('[DB] Failed to fetch tasks');
+    return SendResponse('Failed to find your tasks', 500);
+  }
 }
